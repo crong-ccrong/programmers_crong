85f9ccddd089aeeba907a1d533e33403771912c9
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-09-06 09:45:15 +0900

    [level 2] Title: 숫자의 표현, Time: 0.44 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,12 +1,11 @@
 class Solution {
     public int solution(int n) {
-        int answer = 1;
+        int answer = 0;
         int start = 0;
         int end = 0;
         int sum = 0;
         
-        while (true) {
-            if (end==n) break;
+        while (end<=n) {
             if (sum<n) {
                 sum += ++end;
             } else if (sum==n) {
