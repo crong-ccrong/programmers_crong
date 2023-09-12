4f148ed289116f3be0a89981f2251bdee8c30311
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-09-12 09:44:57 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.01 ms, Memory: 82.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -1,30 +1,33 @@
+import java.util.*;
+
 class Solution {
+    public int eucd (int a, int b) {
+            if (b==0) {
+                return a;
+            }
+            return eucd (b, a%b);
+    }
+    
     public int solution(int a, int b) {
-        int answer = 0;
-        int max = 1;
-        int temp = b;
-        
-        while (a!=0) {
-            max = a;
-            a = b%a;
-            b = max;
-        }
-        
-        temp = temp/max;
-        
-        while (temp%2==0) {
-            temp = temp/2;
-        }
+        int answer = 2;
 
-        while (temp%5==0) {
-            temp = temp/5;
-        }
+        
+        // 분수를 기약분수로 만들기 > 분자와 분모의 최대공약수로 약분하기.
+        int d= b/eucd(a, b);
 
-        if (temp==1) {
-            answer = 1;
+        // 기약분수의 분모의 소인수 구하여 return값 결정.
+        while (d%2==0) {
+            d /=2;
+        }
+        while (d%5==0) {
+            d /=5;
+        }
+        if (d==1) {
+            answer =1;
         } else {
-            answer = 2;
+            answer =2;
         }
+
         return answer;
-    }    
+    }
 }
