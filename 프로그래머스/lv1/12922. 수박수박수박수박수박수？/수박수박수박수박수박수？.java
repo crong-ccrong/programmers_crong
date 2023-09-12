1bfea7e11b6cc67a3938acc7fcb344c8ef01c83c
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-09-12 13:02:55 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.67 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -2,12 +2,7 @@ class Solution {
 	public String solution(int n) {
 		String answer = "";
 		for (int i = 0; i < n; i++) {
-			if(i%2 == 0) {
-				answer += "수";
-			}
-			if(i%2 == 1) {
-				answer += "박";
-			}
+			answer += (i%2==0)?"수":"박";
 		}
 		return answer;
 	}
