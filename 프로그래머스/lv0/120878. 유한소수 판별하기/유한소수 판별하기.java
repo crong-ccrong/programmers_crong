b6f9293fdf3a818f2095e6586c894070a26c83f3
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-09-12 09:41:41 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.java
@@ -2,28 +2,29 @@ class Solution {
     public int solution(int a, int b) {
         int answer = 0;
         int max = 1;
+        int temp = b;
         
-        for (int i = 2; i <= a; i++) {
-			if (a % i == 0 && b % i == 0) {
-				max = i;
-			}
-		}
-        
-        b = b/max;
-        
-        while (b%2==0) {
-            b = b/2;
+        while (a!=0) {
+            max = a;
+            a = b%a;
+            b = max;
         }
         
-        while (b%5==0) {
-            b = b/5;
-        }
+        temp = temp/max;
         
-        if (b==1) {
+        while (temp%2==0) {
+            temp = temp/2;
+        }
+
+        while (temp%5==0) {
+            temp = temp/5;
+        }
+
+        if (temp==1) {
             answer = 1;
         } else {
             answer = 2;
         }
         return answer;
-    }
+    }    
 }
