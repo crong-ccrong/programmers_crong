e3565da8a4e787d9890896bfa0b08c60a9c9c43e
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-10-03 18:51:03 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -4,11 +4,10 @@ class Solution {
         
         // yellow의 가로+세로 길이 구하기
         int length = (brown - 4) / 2;
-        System.out.println(length);
         
+        // 가로x세로 = yellow
         for (int i = 1; i < length; i++) {
             if ((length-i) * i == yellow) {
-                System.out.println(i);
                 answer[0] = length-i+2;
                 answer[1] = i+2;
                 break;
