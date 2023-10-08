9c7cd1e68452ab8115ff4b8474c360360f76ecfa
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-10-08 18:08:32 +0900

    [level 2] Title: 구명보트, Time: 8.96 ms, Memory: 53.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int start = 0;
+        int end = people.length-1;
+        
+        Arrays.sort(people);
+        
+        while(start <= end) {
+            if (people[start] + people[end] <= limit) {
+                start++;
+            }
+            end--;
+            answer++;
+        }
+        return answer;
+    }
+}
