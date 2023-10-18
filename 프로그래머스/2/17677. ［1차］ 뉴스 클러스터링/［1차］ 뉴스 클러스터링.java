97fca4d26dc6fe190d4ebe61385741a1f950862a
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-10-19 00:04:25 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.64 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,6 +1,46 @@
+import java.util.*;
+
 class Solution {
     public int solution(String str1, String str2) {
-        int answer = 0;
+        
+        List<String> list1 = new LinkedList<>();
+		List<String> list2 = new LinkedList<>();
+		List<String> union = new LinkedList<>();
+		List<String> intersection = new LinkedList<>();
+        
+        // 소문자로 바꾸기
+		str1 = str1.toLowerCase();
+		str2 = str2.toLowerCase();
+        
+        // 두 단어씩 끊어서 list에 담기 + 영문자가 아닌 글자쌍은 삭제
+		for (int i = 0; i < str1.length() - 1; i++) {
+			if (str1.charAt(i)>='a' && str1.charAt(i)<='z' && str1.charAt(i+1)>='a' && str1.charAt(i+1)<='z') {
+				list1.add(str1.substring(i, i + 2));
+			}
+		}
+
+		for (int i = 0; i < str2.length() - 1; i++) {
+			if (str2.charAt(i)>='a' && str2.charAt(i)<='z' && str2.charAt(i+1)>='a' && str2.charAt(i+1)<='z') {
+				list2.add(str2.substring(i, i + 2));
+			}
+		}
+        
+        // 교집합, 합집합 구하기
+		for (String str : list1) {
+			if (list2.contains(str)) {
+				list2.remove(str);
+				intersection.add(str);
+			}
+			union.add(str);
+		}
+		
+		for (String str : list2) {
+			union.add(str);
+		}
+        
+        // 자카드 유사도
+        int answer = (union.size() == 0) ? 1*65536 : (int) Math.floor((double)intersection.size()/union.size()*65536);
+        
         return answer;
     }
 }
