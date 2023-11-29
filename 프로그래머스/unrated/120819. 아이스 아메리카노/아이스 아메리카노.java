9c5d29d2989e02a035761ba385bbfb2a91ef4738
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-11-29 13:01:00 +0900

    [level unrated] Title: 아이스 아메리카노, Time: 0.01 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
+++ 프로그래머스/unrated/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int money) {
-        int[] answer = {(int)money/5500, money-(5500*((int)money/5500))};
+        int[] answer = {money/5500, money%5500};
         return answer;
     }
 }
