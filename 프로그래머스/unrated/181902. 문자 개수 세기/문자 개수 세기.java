cf73b3305aaaac90c0f4da7ad9011f152e524d14
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-12-13 00:16:00 +0900

    [level unrated] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/unrated/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,20 +1,14 @@
-import java.util.*;
-
 class Solution {
     public int[] solution(String my_string) {
         int[] answer = new int[52];
-        Map<Character, Integer> map = new HashMap<>();
         
         for (char a : my_string.toCharArray()) {
-            map.put(a, map.getOrDefault(a, 0)+1);
+            if (a >= 'a') {
+                answer[a-'a'+26]++;
+            } else {
+                answer[a-'A']++;
+            }
         }
-        
-        for (char a='A'; a<='Z'; a++) {
-			answer[a-65] = map.getOrDefault(a, 0);
-		}
-		for (char a='a'; a<='z'; a++) {
-			answer[a-71] = map.getOrDefault(a, 0);
-		}
         
         return answer;
     }
