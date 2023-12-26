4199c4824c950532a924416a40b0e1ba9d795707
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2023-12-26 21:43:03 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.27 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -3,17 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int []A, int []B) {
         int answer = 0;
-        Integer[] C = new Integer[B.length];
-        for (int i=0; i<B.length; i++) {
-            C[i] = B[i];
-        }
         
-        // A는 오름차순 정렬, C는 내림차순 정렬
+        // A는 오름차순 정렬, B는 내림차순 정렬
         Arrays.sort(A);
-        Arrays.sort(C, Collections.reverseOrder());
+        Arrays.sort(B);
         
         for (int i=0; i<A.length; i++) {
-            answer += A[i] * C[i];
+            answer += A[i] * B[B.length-1-i];
         }
         return answer;
     }
