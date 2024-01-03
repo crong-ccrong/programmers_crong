9d0d6c009966ae9e37be5df31521298fe95f744c
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-03 10:49:29 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] absolutes, boolean[] signs) {
+        int answer = 0;
+        for (int i=0; i<absolutes.length; i++) {
+            answer = (signs[i]==true) ? answer+absolutes[i] : answer-absolutes[i];
+        }
+        return answer;
+    }
+}
