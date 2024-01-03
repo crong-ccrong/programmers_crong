a288283f907d8b60e75f1efd44a3d03ce27795be
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-03 21:03:19 +0900

    [level 1] Title: 과일 장수, Time: 0.38 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -3,17 +3,13 @@ import java.util.*;
 class Solution {
     public int solution(int k, int m, int[] score) {
         int answer = 0;
-        String[] arr = new String[score.length];
         
         // 점수 순으로 정렬
-        for (int i=0; i<score.length; i++) {
-            arr[i] = score[i]+"";
-        }
-        Arrays.sort(arr, Collections.reverseOrder());
+        Arrays.sort(score);
         
         // 점수 계산
-        for (int i=1; i<=score.length/m; i++) {
-            answer += m*Integer.parseInt(arr[i*m-1]);
+        for (int i=(score.length%m); i<score.length; i+=m) {
+            answer += m * score[i];
         }
         return answer;
     }
