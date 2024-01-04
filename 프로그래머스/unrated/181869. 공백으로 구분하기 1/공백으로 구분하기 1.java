66cd39c4a6eb462c4ff6567b1f6fd3500cc5a7c0
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-05 00:01:06 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.04 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -1,11 +1,6 @@
 class Solution {
     public String[] solution(String my_string) {
-        String[] answer = new String[my_string.length() - my_string.replace(" ", "").length()+1];
-        int index = 0;
-        for (String s : my_string.split(" ")) {
-			answer[index] = s;
-            index++;
-		}
+        String[] answer = my_string.split(" ");
         return answer;
     }
 }
