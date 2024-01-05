6c64318b5a99a3d53af39d653994ceacdb840290
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-05 11:39:23 +0900

    [level unrated] Title: 옹알이 (1), Time: 1.01 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.java
@@ -2,22 +2,11 @@ class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
         
-        for (String s : babbling) {
-			while (s.startsWith("aya") || s.startsWith("ye") || s.startsWith("woo") || s.startsWith("ma")) {
-				if (s.startsWith("aya")) {
-					s = s.substring(3, s.length());
-				} else if (s.startsWith("ye")) {
-					s = s.substring(2, s.length());
-				} else if (s.startsWith("woo")) {
-					s = s.substring(3, s.length());
-				} else { //if (s.startsWith("ma"))
-					s = s.substring(2, s.length());
-				}
-			}
-			if (s.length() == 0) {
-				answer++;
-			}
-		}
+        for(String s : babbling){
+            if(s.matches("^(aya(?!aya)|ye(?!ye)|woo(?!woo)|ma(?!ma))+$")){
+                answer++;
+            }
+        }
         
         return answer;
     }
