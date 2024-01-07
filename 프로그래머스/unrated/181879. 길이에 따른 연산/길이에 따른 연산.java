9b58bea8bdb9e3026b56864e1ced3a10b2373f61
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-07 23:36:42 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.02 ms, Memory: 71.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer;
+        if (num_list.length > 10) {
+            answer = 0;
+            for (int i : num_list) {
+                answer += i;
+            }
+        } else {
+            answer = 1;
+            for (int i : num_list) {
+                answer *= i;
+            }
+        }
+        return answer;
+    }
+}
