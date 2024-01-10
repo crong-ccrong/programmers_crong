62af50ed49273ab2c34dfb8f6f20ccb6bb2792a2
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-11 00:12:42 +0900

    [level unrated] Title: n보다 커질 때까지 더하기, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181884. n보다 커질 때까지 더하기/n보다 커질 때까지 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers, int n) {
+        int answer = 0;
+        for (int i=0; answer<=n; i++) {
+            answer += numbers[i];
+        }
+        return answer;
+    }
+}
