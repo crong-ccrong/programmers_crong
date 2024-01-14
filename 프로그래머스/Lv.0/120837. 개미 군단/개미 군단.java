25bdf4dd27361f8583cd9d9375110a3594fa5642
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-14 17:20:50 +0900

    [level Lv.0] Title: 개미 군단, Time: 0.02 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/Lv.0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/Lv.0/120837. 개미 군단/개미 군단.java
@@ -1,21 +1,5 @@
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        while (hp != 0) {
-            while (hp/5 != 0) {
-                answer += (int)hp/5;
-                hp %= 5;
-            }
-            while (hp/3 != 0) {
-                answer += (int)hp/3;
-                hp %= 3;
-            }
-            while (hp/1 != 0) {
-                answer += (int)hp/1;
-                hp %= 1;
-            }
-        }
-        
-        return answer;
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
     }
 }
