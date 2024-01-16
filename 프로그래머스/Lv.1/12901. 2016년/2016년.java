2459beb9566d5540161e1db2cec783b6dc6f044c
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-16 16:10:42 +0900

    [level Lv.1] Title: 2016년, Time: 1.69 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12901. 2016년/2016년.java
+++ 프로그래머스/Lv.1/12901. 2016년/2016년.java
@@ -5,8 +5,6 @@ class Solution {
     public String solution(int a, int b) {
         LocalDate date = LocalDate.of(2016,a,b);
 		DayOfWeek dayOfWeek = date.getDayOfWeek();
-		int dayOfWeekNumber = dayOfWeek.getValue();
-		String answer = (dayOfWeekNumber==1) ? "MON" : (dayOfWeekNumber==2) ? "TUE" : (dayOfWeekNumber==3) ? "WED" : (dayOfWeekNumber==4) ? "THU" : (dayOfWeekNumber==5) ? "FRI" : (dayOfWeekNumber==6) ? "SAT" : "SUN";
-        return answer;
+        return dayOfWeek.toString().substring(0,3);
     }
 }
