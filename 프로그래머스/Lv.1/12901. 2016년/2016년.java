1beb219ec83790873f2e829cfc98ba51316be4c9
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-16 16:11:30 +0900

    [level Lv.1] Title: 2016년, Time: 3.27 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/Lv.1/12901. 2016년/2016년.java
+++ 프로그래머스/Lv.1/12901. 2016년/2016년.java
@@ -3,8 +3,6 @@ import java.time.LocalDate;
 
 class Solution {
     public String solution(int a, int b) {
-        LocalDate date = LocalDate.of(2016,a,b);
-		DayOfWeek dayOfWeek = date.getDayOfWeek();
-        return dayOfWeek.toString().substring(0,3);
+        return LocalDate.of(2016,a,b).getDayOfWeek().toString().substring(0,3);
     }
 }
