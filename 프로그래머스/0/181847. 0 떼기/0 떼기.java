f2f674b9f793705bc9e6a1b9377fd5e2f0498eaf
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-17 23:29:36 +0900

    [level 0] Title: 0 떼기, Time: 7.41 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,8 +1,5 @@
 class Solution {
     public String solution(String n_str) {
-        while (n_str.startsWith("0")) {
-            n_str = n_str.substring(1, n_str.length());
-        }
-        return n_str;
+        return Integer.parseInt(n_str)+"";
     }
 }
