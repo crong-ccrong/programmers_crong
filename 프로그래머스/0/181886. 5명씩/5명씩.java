914a668e18a6cc1de8ef9a55aa3bec589accaedf
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-17 23:40:03 +0900

    [level 0] Title: 5명씩, Time: 0.01 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -3,10 +3,7 @@ class Solution {
         int length = (names.length%5==0) ? names.length/5 : names.length/5+1;
         String[] answer = new String[length];
         for (int i=0; i<answer.length; i++) {
-            int index = i*5;
-            if (index < names.length) {
-                answer[i] = names[index];
-            }
+            answer[i] = names[i*5];
         }
         return answer;
     }
