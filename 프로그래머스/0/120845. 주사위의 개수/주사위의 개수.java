87fa090e5675ad2b2f82e91e126dfe6e0397ec80
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-19 15:30:32 +0900

    [level 0] Title: 주사위의 개수, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
+++ 프로그래머스/0/120845. 주사위의 개수/주사위의 개수.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(int[] box, int n) {
-        int answer = 1;
-        for (int i : box) {
-            answer *= i/n;
-        }
-        return answer;
+        return (box[0]/n)*(box[1]/n)*(box[2]/n);
     }
 }
