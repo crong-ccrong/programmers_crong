f637199cb22b306464debf40f4cbb1a627a3bbe2
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-21 21:51:16 +0900

    [level 0] Title: l로 만들기, Time: 15.17 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        for (char c : myString.toCharArray()) {
-            answer += (c>='a' && c<'l') ? "l" : c+"";
-        }
-        return answer;
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
