6963a6be1ac31257f8cfd3a99f8db61ef423e0d5
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-01-28 01:26:55 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 66.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 45;
+        for (int i : numbers) {
+            answer -= i;
+        }
+        return answer;
+    }
+}
