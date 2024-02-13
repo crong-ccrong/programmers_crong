32267fa70d51a64bf4d93c97fdc60e52319dfaae
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-02-13 18:52:11 +0900

    [level 0] Title: 숫자 찾기, Time: 6.05 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(int num, int k) {
-        return (num+"").contains(k+"") ? (num+"").indexOf(k+"")+1 : -1;
+        return (" "+num).indexOf(k+"");
     }
 }
