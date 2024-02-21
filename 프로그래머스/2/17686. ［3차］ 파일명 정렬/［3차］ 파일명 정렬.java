40429d6690e7303246f436f948c74970d560f1cd
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-02-21 17:03:05 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 24.94 ms, Memory: 97.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] files) {
+        List<String> answer = new ArrayList<>();
+        Map<String, List<String>> headMap = new HashMap<>();
+        Map<Integer, List<String>> numberMap = new HashMap();
+        
+        // head로 정렬
+        for (String str : files) {
+			String head = str.split("(?<=\\D)(?=\\d)")[0].toLowerCase();
+			List<String> list = headMap.getOrDefault(head, new ArrayList<>());
+			list.add(str);
+			headMap.put(head, list);
+		}
+		List<String> headKeyList = new ArrayList<>(headMap.keySet());
+		Collections.sort(headKeyList);
+        
+        // number로 정렬
+        for (String str : headKeyList) {
+			for (String s : headMap.get(str)) {
+				int number = Integer.parseInt(s.replace(" ", "").replace(".", "").replace("-", "").split("\\D+")[1]);
+				List<String> list = numberMap.getOrDefault(number, new ArrayList<>());
+				list.add(s);
+				numberMap.put(number, list);
+			}
+			
+			List<Integer> numberKeyList = new ArrayList<>(numberMap.keySet());
+			Collections.sort(numberKeyList);
+			for (int i : numberKeyList) {
+				for (String element : numberMap.get(i)) {
+					answer.add(element);
+				}
+			}
+			numberMap.clear();
+		}
+        
+        return answer.toArray(new String[answer.size()]);
+    }
+}
