25c72c4203c3877b40cf542bdb655ef7067d4aaf
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-02-24 05:19:58 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1477.77 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -9,15 +9,9 @@ class Solution {
         
         for (int i=1; i<=elements.length; i++) { // 길이
             for (int j=0; j<elements.length; j++) { // 시작 인덱스
-                int k = 0; // 길이 확인
                 int sum = 0;
-                while (true) {
-                    sum += arr[j+k];
-                    k++;
-                    if (k == i) {
-                        set.add(sum);
-                        break;
-                    }
+                for (int k=j; k<i+j; k++) {
+                    sum += elements[k%elements.length];
                 }
                 set.add(sum);
             }
