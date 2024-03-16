218a8c5f9bf6ca029ae604fc19f250f8956ef3f4
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-03-16 15:42:16 +0900

    [level 2] Title: 주식가격, Time: 21.45 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-    Stack<Integer> stack = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
     
-    for (int i = 0; i < prices.length; i++) {
-        while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-            int index = stack.pop();
-            answer[index] = i - index;
+        for (int i = 0; i < prices.length; i++) {
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int index = stack.pop();
+                answer[index] = i - index;
+            }
+            stack.push(i);
         }
-        stack.push(i);
-    }
-    while (!stack.isEmpty()) {
-        int index = stack.pop();
-        answer[index] = prices.length - index - 1;
-    }
-    return answer;
+        while (!stack.isEmpty()) {
+            int index = stack.pop();
+            answer[index] = prices.length - index - 1;
+        }
+        return answer;
     }
 }
