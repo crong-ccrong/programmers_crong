5b901193ede256e20c7cff533d00a0565c55accf
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-03-20 06:15:58 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 9.08 ms, Memory: 77.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int[] arry : queries) {
+            for (int i=0; i<arr.length; i++) {
+                arr[i] = (i>=arry[0] && i<=arry[1] && i%arry[2]==0) ? arr[i]+1 : arr[i];
+            }
+            System.out.println();
+        }
+        return arr;
+    }
+}
