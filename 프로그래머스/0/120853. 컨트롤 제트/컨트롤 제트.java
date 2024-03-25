be64aa146d60b6742ff9c91b9cd675afaea3b61c
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-03-25 16:46:29 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.18 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] arr = s.split(" ");
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i].equals("Z")) {
+                answer -= Integer.parseInt(arr[i-1]);
+            } else {
+                answer += Integer.parseInt(arr[i]);
+            }
+        }
+        return answer;
+    }
+}
