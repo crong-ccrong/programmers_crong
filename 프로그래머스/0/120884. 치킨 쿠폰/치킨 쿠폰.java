7c1c1bc5ef63db7fdbb4402c5ab10859625b3dfd
crong-ccrong <109481825+crong-ccrong@users.noreply.github.com>
2024-04-27 00:53:56 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int chicken) {
+        int answer = 0;
+        while (chicken >= 10) {
+            answer += chicken/10;
+            chicken = chicken/10 + chicken%10;
+        }
+        return answer;
+    }
+}
